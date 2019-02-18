c108411c1f4e487c05de6ac1859ff7642f31b795
Thiago <skiptirengu@users.noreply.github.com>
2019-02-18 17:10:11 -0300

    Fix small typo

--- NetCore.AutoRegisterDi/AutoRegisterHelpers.cs
+++ NetCore.AutoRegisterDi/AutoRegisterHelpers.cs
@@ -45,7 +45,7 @@ namespace NetCore.AutoRegisterDi
         /// This registers the classes against any public interfaces (other than IDisposable) implemented by the class
         /// </summary>
         /// <param name="autoRegData">AutoRegister data produced by <see cref="RegisterAssemblyPublicNonGenericClasses"/></param> method
-        /// <param name="lifetime">Allows you to define the timetime of the service - defaults to ServiceLifetime.Transient</param>
+        /// <param name="lifetime">Allows you to define the lifetime of the service - defaults to ServiceLifetime.Transient</param>
         /// <returns></returns>
         public static IServiceCollection AsPublicImplementedInterfaces(this AutoRegisterData autoRegData, 
             ServiceLifetime lifetime = ServiceLifetime.Transient)
